a3210234ec105c96585a25759a997dda09d52337
DariCari <38152878+dariusptrs@users.noreply.github.com>
2024-03-26 17:42:27 +0100

    update CI
    Update ci.yml
    
    fix build error in cheatsheet_example.tex
    
    Update write-gitid.sh

---
+++ .github/pull_request_template.md
@@ -0,0 +1,14 @@
+---
+name: Vorschlag Fehlerkorrektur
+about: Du hast einen Fehler gefunden und willst ihn korrigieren
+title: ''
+labels: bug
+assignees: ''
+
+---
+
+**Fehlerbeschreibung**
+Beschreibe hier den Fehler
+
+**Referenz**
+Wenn möglich immer eine Referenz zum Skript o.ä. angeben oder als Screenshot mit einfügen.
--- write-gitid.sh
+++ write-gitid.sh
@@ -12,3 +12,12 @@ fi
 GITREV=$(git rev-list HEAD --count)
 echo "\\def\\GitRevision{$GITREV}"> git.id
 git show -s --format=%ci | xargs -I var_d $DATEBIN -d "var_d" +\\def\\GitNiceDate{%-d.\ %B\ %Y\ um\ %H:%M\ Uhr} >> git.id
+
+# Get the repository's remote URL
+REPO_URL=$(git remote get-url origin)
+
+# Convert SSH URL to HTTPS URL and append /issues for the issues page
+# This also ensures it works for URLs ending with .git and without
+ISSUES_URL=$(echo $REPO_URL | sed -e 's/^git@github\.com:/https:\/\/github\.com\//' -e 's/\.git$//' -e 's/$/\/issues/')
+
+echo "\\def\\GitIssuesURL{$ISSUES_URL}" >> git.id
